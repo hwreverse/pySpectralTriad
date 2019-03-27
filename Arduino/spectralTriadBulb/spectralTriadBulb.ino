c919f8ef5b8cce1dc44ad39df79313714a9116d2
hwreverse <49004465+hwreverse@users.noreply.github.com>
2019-03-28 00:45:17 +0300

    Create spectralTriadBulb.ino

---
+++ Arduino/spectralTriadBulb/spectralTriadBulb.ino
@@ -0,0 +1,64 @@
+// Code works in Arduino and on Teensy (first I2C=19,20)
+#include "SparkFun_AS7265X.h" //Get the Library with the Librarymanager !
+AS7265X sensor;
+
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  if(sensor.begin() == false)
+  {
+    Serial.println("Sensor does not appear to be connected. Please check wiring. Freezing...");
+    while(1);
+  }
+
+  sensor.disableIndicator(); //Turn off the blue status LED
+  //put some triple ductttape over the red diode, or use a bit of play dough to cover it !!!
+
+}
+
+void loop() {
+  sensor.takeMeasurementsWithBulb();
+  //sensor.takeMeasurements();
+  
+  Serial.print(sensor.getCalibratedA());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedB());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedC());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedD());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedE());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedF());
+  Serial.print(",");
+
+  Serial.print(sensor.getCalibratedG());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedH());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedI());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedJ());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedK());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedL());
+  Serial.print(",");
+
+  Serial.print(sensor.getCalibratedR());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedS());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedT());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedU());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedV());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedW());
+
+  Serial.println();
+}
