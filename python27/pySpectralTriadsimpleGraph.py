a21ae07eb541456fc4f6dd59109f9024f00f930e
HWR0 <49004465+hwreverse@users.noreply.github.com>
2019-03-29 04:05:24 +0300

    Update pySpectralTriadsimpleGraph.py

--- python27/pySpectralTriadsimpleGraph.py
+++ python27/pySpectralTriadsimpleGraph.py
@@ -14,13 +14,27 @@ from serial import Serial
  
 spectreData = serial.Serial("COM7", 115200) #Cchange COM7 to the right port - under Linux it will look more like "/dev/ttyACMx"... 
  
+loop=True
+
 spectreReadings = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,] 
 defaultYLimit = 1023
- 
+
+fig = plt.figure() 
 plt.ion()     
+
+def handle_close(evt):
+	spectreData.reset_input_buffer()
+	plt.ioff()
+	plt.close()
+	global loop
+	loop=False
+	#print("close event")
+	#exit()
+	#sys.exit()
  
  
-def doPlot():     
+def doPlot():  
+	fig.canvas.mpl_connect('close_event', handle_close) # figured out finally how to close the "groundhog" window
 	plt.ylim(0,defaultYLimit)                            
 	plt.title('Spectral Response')           
 	plt.grid(True)                              
@@ -28,7 +42,7 @@ def doPlot():
 	plt.plot(spectreReadings, 'ro-', label='Spectral readings')       
 	plt.legend(loc='upper left')                
 	 
-while True:            
+while (loop):            
 	while (spectreData.inWaiting()== 0):        
 		pass            
 	spectreString = spectreData.readline()
@@ -39,5 +53,8 @@ while True:
 			
 		defaultYLimit=max(spectreReadings)+2 # cosmetics , just used to
 		drawnow(doPlot)
+
+print(".")
+
 		
 	
